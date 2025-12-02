0146c477402a5b296e1ed3fa5ad0245a8cd59bb1
MadhuriGhadge <ghadgemadhuri92@gmail.com>
2025-12-02 18:26:38 +0530

    Remove phase1 folder from main

--- phase1/gradio/demo.py
+++
@@ -1,9 +0,0 @@
-
-def greet(name):
-    return f"Hello, {name}!"
-
-import gradio as gr
-
-demo = gr.Interface(fn=greet, inputs="text",outputs="text")
-demo.launch()
-
--- phase1/streamlit/task4.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-def update_name():
-    st.session_state.name_updated = st.session_state.name_input.upper()
-
-st.text_input("enter name:",key="name_input",on_change=update_name)
-
-st.write("original:",st.session_state.get("name_input"))
-st.write("updated:",st.session_state.get("name_updated"))
